7a3a8e0616a4af7ca4d6e13ed45851d5114c24af
Benoit Jadinon <benoit@skipr.co>
2023-03-24 21:27:47 +0100

    native callback + cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,15 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
+// illustrates a quick fix idea that would hide the native back button on subsequent web pages
+const bool quickFixByJustHidingNativeButton = false;
+
+// url to change to the test one
+String get urlToLoadInitially =>
+    "https://benoitskipr.github.io/${quickFixByJustHidingNativeButton ? "poc-noback.html" : "poc.html"}";
+const String jsBackButtonCallback = "nativeBack";
+
+//
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -11,7 +20,7 @@ Future main() async {
     await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
   }
 
-  runApp(const MaterialApp(home: Webview()));
+  runApp(const MaterialApp(home: HomePage()));
 }
 
 class HomePage extends StatelessWidget {
@@ -20,29 +29,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'HOMEPAGE',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.grey.shade100,
+        shadowColor: Colors.transparent,
+      ),
       body: Center(
-        child: MaterialButton(
+        child: ElevatedButton(
           onPressed: () => Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => const Webview()),
+            MaterialPageRoute(builder: (context) => const WebView()),
           ),
+          style: ElevatedButton.styleFrom(
+              elevation: 12.0, textStyle: const TextStyle(color: Colors.white)),
+          child: const Text('open the webview'),
         ),
       ),
     );
   }
 }
 
-class Webview extends StatefulWidget {
-  const Webview({super.key});
+class WebView extends StatefulWidget {
+  const WebView({super.key});
 
   @override
-  createState() => _WebviewState();
+  createState() => _WebViewState();
 }
 
-class _WebviewState extends State<Webview> {
+class _WebViewState extends State<WebView> {
   final GlobalKey webViewKey = GlobalKey();
 
-  InAppWebViewController? webViewController;
+  InAppWebViewController? _webViewController;
   InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
       crossPlatform: InAppWebViewOptions(
         useShouldOverrideUrlLoading: true,
@@ -57,194 +77,46 @@ class _WebviewState extends State<Webview> {
         allowsInlineMediaPlayback: true,
       ));
 
-  late PullToRefreshController pullToRefreshController;
-  String url = "";
   double progress = 0;
-  final urlController = TextEditingController();
 
-  bool showBackButton = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    pullToRefreshController = PullToRefreshController(
-      options: PullToRefreshOptions(
-        color: Colors.blue,
-      ),
-      onRefresh: () async {
-        if (Platform.isAndroid) {
-          webViewController?.reload();
-        } else if (Platform.isIOS) {
-          webViewController?.loadUrl(
-              urlRequest: URLRequest(url: await webViewController?.getUrl()));
-        }
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
+  // is updated by the webView controller on page change
+  bool hasHistory = false;
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async {
-        if (await (webViewController?.canGoBack() ?? Future.value(false))) {
-          var webHistory = await webViewController?.getCopyBackForwardList();
-          if ((webHistory?.currentIndex ?? 0) <= 1) {
-            return true;
-          }
-          await webViewController?.goBack();
-          return false;
-        }
-        return true;
-      },
+      onWillPop: _interceptNativeBackButton,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("InAppWebView test"),
-          leading: showBackButton
-              ? BackButton(
-                  onPressed: () {
-                    webViewController?.goBack();
-                  },
-                )
-              : null,
-        ),
+        appBar: quickFixByJustHidingNativeButton
+            ? AppBar(
+                backgroundColor: Colors.grey.shade100,
+                shadowColor: Colors.transparent,
+                leading: (quickFixByJustHidingNativeButton && hasHistory)
+                    ? null
+                    : BackButton(
+                        color: Colors.blue,
+                        onPressed: hasHistory
+                            ? () => _webViewController?.goBack()
+                            : null,
+                      ),
+              )
+            : null,
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              SearchField(
-                urlController: urlController,
-                webViewController: webViewController,
-              ),
               Expanded(
                 child: Stack(
                   children: [
-                    InAppWebView(
-                      key: webViewKey,
-                      /*initialUrlRequest:
-                          URLRequest(url: Uri.parse("https://inappwebview.dev/"),
-                      ),*/
-                      // Loads html
-                      // but first page does not count in the history, better use an external page
-                      initialData: InAppWebViewInitialData(
-                        data: '<html>'
-                            '<head>'
-                            '<meta name="viewport" content="initial-scale=1.0" />'
-                            '</head>'
-                            '<button onclick=history.back()">history.back()</button>'
-                            '<button onclick="location.href = \'https://www.google.com\'">navigate out</button>'
-                            '<a href="https://www.google.com">navigate out</a>'
-                            '</html>',
-                      ),
-                      initialOptions: options,
-                      pullToRefreshController: pullToRefreshController,
-                      onWebViewCreated: (controller) {
-                        webViewController = controller;
-                      },
-                      onLoadStart: (controller, url) {
-                        setState(() async {
-                          this.url = url.toString();
-                          urlController.text = this.url;
-
-                          //
-                          showBackButton = await controller.canGoBack();
-                        });
-                      },
-                      androidOnPermissionRequest:
-                          (controller, origin, resources) async {
-                        return PermissionRequestResponse(
-                            resources: resources,
-                            action: PermissionRequestResponseAction.GRANT);
-                      },
-                      shouldOverrideUrlLoading:
-                          (controller, navigationAction) async {
-                        /*var uri = navigationAction.request.url!;
-                        if (![
-                          "http",
-                          "https",
-                          "file",
-                          "chrome",
-                          "data",
-                          "javascript",
-                          "about",
-                          // get the list of supported ones from info.plist
-                        ].contains(uri.scheme)) {
-                          final uri = Uri.parse(url);
-                          if (await canLaunchUrl(uri)) {
-                            // Launch the App
-                            await launchUrl(uri);
-                            // and cancel the request
-                            return NavigationActionPolicy.CANCEL;
-                          }
-                        }
-                        */
-                        return NavigationActionPolicy.ALLOW;
-                      },
-                      onLoadStop: (controller, url) async {
-                        pullToRefreshController.endRefreshing();
-                        setState(() {
-                          this.url = url.toString();
-                          urlController.text = this.url;
-                        });
-                      },
-                      onLoadError: (controller, url, code, message) {
-                        pullToRefreshController.endRefreshing();
-                      },
-                      onProgressChanged: (controller, progress) {
-                        if (progress == 100) {
-                          pullToRefreshController.endRefreshing();
-                        }
-                        setState(() {
-                          this.progress = progress / 100;
-                          urlController.text = url;
-                        });
-                      },
-                      onUpdateVisitedHistory:
-                          (controller, url, androidIsReload) {
-                        setState(() async {
-                          this.url = url.toString();
-                          urlController.text = this.url;
-
-                          //
-                          showBackButton = await controller.canGoBack();
-                        });
-                      },
-                      onConsoleMessage: (controller, consoleMessage) {
-                        debugPrint(consoleMessage.message);
-                      },
-                    ),
+                    _buildWebView(),
                     progress < 1.0
                         ? LinearProgressIndicator(value: progress)
-                        : Container(),
+                        : const SizedBox.shrink(),
                   ],
                 ),
               ),
-              /*ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(
-                    child: const Icon(Icons.arrow_back),
-                    onPressed: () {
-                      webViewController?.goBack();
-                    },
-                  ),
-                  ElevatedButton(
-                    child: const Icon(Icons.arrow_forward),
-                    onPressed: () {
-                      webViewController?.goForward();
-                    },
-                  ),
-                  ElevatedButton(
-                    child: const Icon(Icons.refresh),
-                    onPressed: () {
-                      webViewController?.reload();
-                    },
-                  ),
-                ],
+              /*ArrowBar(
+                hasHistory: hasHistory,
+                webViewController: _webViewController,
               ),*/
             ],
           ),
@@ -252,31 +124,124 @@ class _WebviewState extends State<Webview> {
       ),
     );
   }
+
+  Widget _buildWebView() {
+    return InAppWebView(
+      key: webViewKey,
+      initialUrlRequest: URLRequest(
+        url: Uri.parse(urlToLoadInitially),
+      ),
+      // or it can load html but that first page does not count
+      // in the history,
+      // better to use an external page with initialUrlRequest
+      /*initialData: InAppWebViewInitialData(
+        data: '<html>'
+            '<head>'
+            '<meta name="viewport" content="initial-scale=1.0" />'
+            '</head>'
+            '<button onclick=history.back()">history.back()</button>'
+            '<button onclick="location.href = \'https://www.google.com\'">navigate out</button>'
+            '<a href="https://www.google.com">navigate out</a>'
+            '</html>',
+      ),*/
+      initialOptions: options,
+      onWebViewCreated: (controller) {
+        _webViewController = controller;
+        _webViewController!.addJavaScriptHandler(
+          handlerName: jsBackButtonCallback,
+          callback: (_) => Navigator.pop(context),
+        );
+      },
+      androidOnPermissionRequest: (controller, origin, resources) async {
+        return PermissionRequestResponse(
+            resources: resources,
+            action: PermissionRequestResponseAction.GRANT);
+      },
+      shouldOverrideUrlLoading: (controller, navigationAction) async =>
+          NavigationActionPolicy.ALLOW,
+      onLoadStop: (controller, url) async {
+        hasHistory = await _webViewController!.canGoBack();
+        setState(() {});
+      },
+      onLoadError: (controller, url, code, message) {},
+      onProgressChanged: (controller, progress) {
+        setState(() => this.progress = progress / 100);
+      },
+      onConsoleMessage: (controller, consoleMessage) {
+        debugPrint(consoleMessage.message);
+        _showingConsoleAsAlertDialog(context, consoleMessage.message);
+      },
+    );
+  }
+
+  // WillPopScope intercepts the physical back button press on Android
+  // it navigates back inside the webview if this one has history,
+  // otherwise goes back to the previous native page before this webview page
+  Future<bool> _interceptNativeBackButton() async {
+    if (await (_webViewController?.canGoBack() ?? Future.value(false))) {
+      var webHistory = await _webViewController?.getCopyBackForwardList();
+      if ((webHistory?.currentIndex ?? 0) <= 1) {
+        return true;
+      }
+      await _webViewController?.goBack();
+      return false;
+    }
+    return true;
+  }
+
+  void _showingConsoleAsAlertDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text("Alert"),
+        content: Text(message),
+        actions: [
+          TextButton(
+            child: const Text("OK"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class SearchField extends StatelessWidget {
-  const SearchField({
+/*
+class ArrowBar extends StatelessWidget {
+  const ArrowBar({
     Key? key,
-    required this.urlController,
+    required this.hasHistory,
     required this.webViewController,
   }) : super(key: key);
 
-  final TextEditingController urlController;
+  final bool hasHistory;
   final InAppWebViewController? webViewController;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: const InputDecoration(prefixIcon: Icon(Icons.search)),
-      controller: urlController,
-      keyboardType: TextInputType.url,
-      onSubmitted: (value) {
-        var url = Uri.parse(value);
-        if (url.scheme.isEmpty) {
-          url = Uri.parse("https://www.google.com/search?q=$value");
-        }
-        webViewController?.loadUrl(urlRequest: URLRequest(url: url));
-      },
+    return ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: hasHistory ? () => webViewController?.goBack() : null,
+          child: const Icon(Icons.arrow_back),
+        ),
+        ElevatedButton(
+          child: const Icon(Icons.arrow_forward),
+          onPressed: () {
+            webViewController?.goForward();
+          },
+        ),
+        ElevatedButton(
+          child: const Icon(Icons.refresh),
+          onPressed: () {
+            webViewController?.reload();
+          },
+        ),
+      ],
     );
   }
 }
+*/
